a773c8b8d4e81af37bd63c2e927d7cde0494a347
Pablo R <pcrg1941@gmail.com>
2024-12-04 16:11:31 -0500

    Adding code outline and documentation for c1m1 assesment, following guidelines

--- stats.c
+++ stats.c
@@ -1,25 +1,34 @@
+/**************************************************************************/ /**
+                                                                              * @file stats.h
+                                                                              * @brief Example C programming File
+                                                                              *
+                                                                              * @author <Pablo Rodriguez>
+                                                                              * @date 2024-12-04
+                                                                              *
+                                                                              * A simple C-Programming example that exhibits a handful of basic c-programming
+                                                                              * features to show how to calculate some statistics on a set of numbers:
+                                                                              *     -Mean
+                                                                              *     -Maximum
+                                                                              *     -Minimum
+                                                                              *     -Median
+                                                                              *
+                                                                              * On the other hand, is implemented a sort function to organize data from
+                                                                              * largest to smallest value.
+                                                                              *
+                                                                              *    Note: Is an example to learn concepts about functions, operators and sw
+                                                                              *          tools.
+                                                                              *
+                                                                              *****************************************************************************/
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
-
-
 
 #include <stdio.h>
 #include "stats.h"
@@ -27,17 +36,30 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+void main()
+{
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+  unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+                              114, 88, 45, 76, 123, 87, 25, 23,
+                              200, 122, 150, 90, 92, 87, 177, 244,
+                              201, 6, 12, 60, 8, 2, 5, 67,
+                              7, 87, 250, 230, 99, 3, 100, 90};
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
-
 }
 
 /* Add other Implementation File Code Here */
+void sort_array(unsigned char *ptr_numbers, unsigned char length_ptr) {}
+
+void print_statistics(unsigned char *ptr_numbers, unsigned char length_ptr) {}
+
+void print_array(unsigned char *ptr_numbers, unsigned char length_ptr) {}
+
+unsigned char find_minimum(unsigned char *ptr_numbers, unsigned char length_ptr) {}
+
+unsigned char find_maximum(unsigned char *ptr_numbers, unsigned char length_ptr) {}
+
+unsigned char find_median(unsigned char *ptr_numbers, unsigned char length_ptr) {}
+
+unsigned char find_mean(unsigned char *ptr_numbers, unsigned char length_ptr) {}
--- stats.h
+++ stats.h
@@ -1,3 +1,24 @@
+/**************************************************************************//**
+ * @file stats.h
+ * @brief Example C programming File
+ * 
+ * @author <Pablo Rodriguez>
+ * @date 2024-12-04
+ *
+ * A simple C-Programming example that exhibits a handful of basic c-programming
+ * features to show how to calculate some statistics on a set of numbers:
+ *     -Mean
+ *     -Maximum
+ *     -Minimum
+ *     -Median
+ * 
+ * On the other hand, is implemented a sort function to organize data from
+ * largest to smallest value.
+ *
+ *    Note: Is an example to learn concepts about functions, operators and sw
+ *          tools.
+ *
+ *****************************************************************************/
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
@@ -8,33 +29,92 @@
  * misuse of this material. 
  *
  *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
+/******************************************************************************
+ * Function: find_mean
+ * Description:
+ *     This function calculates the mean value of data array.
+ * Parameters:
+ *     int * ptr_numbers: Pointer to a data set
+ *     int length_ptr: Number of items in data set
+ * Return: 
+ *     Mean value of the numbers provided.
+ *****************************************************************************/
+unsigned char find_mean(unsigned char * ptr_numbers, unsigned char length_ptr);
+/******************************************************************************
+ * Function: find_median
+ * Description:
+ *     This function calculates the median value of data array.
+ * Parameters:
+ *     int * ptr_numbers: Pointer to a data set
+ *     int length_ptr: Number of items in data set
+ * Return: 
+ *     Median value of the numbers provided.
+ *****************************************************************************/
+unsigned char find_median(unsigned char * ptr_numbers, unsigned char length_ptr);
+/******************************************************************************
+ * Function: find_maximum
+ * Description:
+ *     This function calculates the maximum value of data array.
+ * Parameters:
+ *     int * ptr_numbers: Pointer to a data set
+ *     int length_ptr: Number of items in data set
+ * Return: 
+ *     Maximum value of the numbers provided.
+ *****************************************************************************/
+unsigned char find_maximum(unsigned char * ptr_numbers, unsigned char length_ptr);
+/******************************************************************************
+ * Function: find_minimum
+ * Description:
+ *     This function calculates the minimum value of data array.
+ * Parameters:
+ *     int * ptr_numbers: Pointer to a data set
+ *     int length_ptr: Number of items in data set
+ * Return: 
+ *     Minimum value of the numbers provided.
+ *****************************************************************************/
+unsigned char find_minimum(unsigned char * ptr_numbers, unsigned char length_ptr);
 
+/******************************************************************************
+ * Function: print_array
+ * Description:
+ *     This function prints data array in screen.
+ * Parameters:
+ *     int * ptr_numbers: Pointer to a data set
+ *     int length_ptr: Number of items in data set
+ * Return: 
+ *     Empty.
+ *****************************************************************************/
+void print_array(unsigned char * ptr_numbers, unsigned char length_ptr);
+
+/******************************************************************************
+ * Function: print_statistics
+ * Description:
+ *     This function prints statistics as minimum, maximum, mean, and median
+ *     of data array in screen.
+ * Parameters:
+ *     int * ptr_numbers: Pointer to a data set
+ *     int length_ptr: Number of items in data set
+ * Return: 
+ *     Empty.
+ *****************************************************************************/
+void print_statistics(unsigned char * ptr_numbers, unsigned char length_ptr);
+
+/******************************************************************************
+ * Function: sort_array
+ * Description:
+ *     This function sorts data array from largest to smallest value.
+ * Parameters:
+ *     int * ptr_numbers: Pointer to a data set
+ *     int length_ptr: Number of items in data set
+ * Return: 
+ *     Empty.
+ *****************************************************************************/
+void sort_array(unsigned char * ptr_numbers, unsigned char length_ptr);
 
 #endif /* __STATS_H__ */
